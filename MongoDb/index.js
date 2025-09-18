e5c31319c2b340770cc6881ce2b74e10d3e83f24
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2025-09-18 14:14:50 +0530

    add: connect db to postman

--- MongoDb/index.js
+++ MongoDb/index.js
@@ -1,20 +1,44 @@
+const express = require("express")
 const mongoose = require("mongoose")
+
+const app = express();
+
+app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/user_app")
 
-const User = mongoose.model('Users', {name: String, email: String, password: String });
+const User = mongoose.model('Users', {
+    name: String, 
+    email: String, 
+    password: String 
+});
 
-// CRUD:- create, read, update, delete
+app.post("/signup", async (req, res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
 
-// read user
-const existingUser = await User.findOne({email})
+    // CRUD => create, read, update, delete
+
+    // read user
+    // User.delete User.update
+    const existingUser = await User.findOne({ email: username })
 
 
-//create user
-const user = new User({
-    name: 'imanshu Kumar',
-    email: 'himansu@gmial.com',
-    password: '123429'
+    if(existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+
+    //create user
+    const user = new User({
+        name: username,
+        email: username,
+        password: password 
+    })
+
+    user.save();
+    res.json({
+        "msg" : "User created succefully"
+    })
 })
-
-user.save();
+app.listen(3000); 
