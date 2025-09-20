8506d36c10d4abf4206e24936d7cb4fe7415c39c
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2025-09-20 17:32:37 +0530

    add: examples'

--- Middle_WARES/index2.js
+++ Middle_WARES/index2.js
@@ -1,4 +1,32 @@
+const express = require("express")
+const app = express();
 
 
+function isOldEnough(req, res, next) {
+    const age = req.query.age;
 
-app.listen(3001);
+    if(age >= 14) {
+        next();
+    }else {
+        res.json({
+            msg : "sorry  you are not of age yet"
+        });
+    }
+}
+
+// or can be use this as middile ware
+//app.use(isOldEnough);
+
+app.get("/ride1", isOldEnough, (req, res)=> {
+    res.json({
+        msg : "You have successfully riden by the ride1"
+    }); 
+});
+
+app.get("/ride2", isOldEnough, (req, res)=> {
+    res.json({
+        msg : "You have successfully riden by the ride1"
+    })
+});
+
+app.listen(3000);
