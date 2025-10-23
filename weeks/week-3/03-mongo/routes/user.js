f2f85abffee920ede9ca660cd869465092d26ac7
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2025-10-24 01:41:57 +0530

    add: purchased course route

--- weeks/week-3/03-mongo/routes/user.js
+++ weeks/week-3/03-mongo/routes/user.js
@@ -50,8 +50,21 @@ router.post('/courses/:courseId', userMiddleware, (req, res) => {
     })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const user = await User.findOne({
+        username: req.headers.username,
+    });
+
+    console.log(user.purchasedCourses);
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    })
+     res.json({
+        courses: courses
+     })
      
 });
 
