b5e0936332955bb242d6242510e66ded9c88d115
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2026-01-06 21:39:28 +0530

    add: re-render and return stat

--- react/week-6-exp/src/App.jsx
+++ react/week-6-exp/src/App.jsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
-function App() {
-  const [title, setTitle] = useState("Himanshu");
 
-  function updateTitle() {
-    setTitle("Updated Title");
-  }
+function App() {
 
   return (
     <div>
-      <button onClick={updateTitle}>Update the title</button>
-      <Header title={title} />
+      <HeaderWithButton />
       <Header title="Hello Linux2" />
     </div>
   )
 }
 
+function HeaderWithButton() {
+  const [title, setTitle] = useState("Himanshu");
+
+  function updateTitle() {
+    setTitle("My name is " + Math.random());
+  }
+
+  return <div>
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title} />
+
+    </div>
+  
+}
+
 function Header(props) {
   return <div>
-    {props.title}9
+    {props.title}
   </div>
 }
 
