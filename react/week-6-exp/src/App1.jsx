d3abe65503efbc3776b15d948e17f1da6ebbe195
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2026-01-11 00:01:06 +0530

    add: todo app

---
+++ react/week-6-exp/src/App1.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useState } from 'react'
+
+let counter = 4;
+
+function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, title: "Learn React", description: "learn react from scratch"},
+    {id: 2, title: "Learn DSA", description: "learn DSA from scratch"},
+    {id: 3, title: "Go To GYm", description: "Go To GYm daily"},
+  ])
+
+
+  function addTodo() {
+    setTodos([...todos, {
+      id: counter++,
+      title: Math.random().toString(),
+      description: Math.random() 
+    }])
+  }
+
+  return (
+    <div>
+      <button onClick = {addTodo}>Add Todo</button>
+      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+
+    </div>
+  )
+}
+
+
+
+function Todo({title, description}) {
+  return <div>
+    <h1>{title}</h1>
+    <h5>{description}</h5>
+  </div>
+}
+
+
+
+
+
+
+
+// function HeaderWithButton() {
+//   const [title, setTitle] = useState("Himanshu");
+
+//   function updateTitle() {
+//     setTitle("My name is " + Math.random());
+//   }
+
+//   return <div>
+//       <button onClick={updateTitle}>Update the title</button>
+//       <Header title={title} />
+
+//     </div>
+  
+// }
+
+// function Header(props) {
+//   return <div>
+//     {props.title}
+//   </div>
+// }
+
+
+export default App
